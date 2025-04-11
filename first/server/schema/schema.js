b1c283f8828e1b0d1ce50f95b5f107f570b81de2
uzuw <uzuwxd@gmail.com>
2025-04-11 22:32:00 +0545

    to get a cloient details with some id

--- first/server/schema/schema.js
+++ first/server/schema/schema.js
@@ -1,3 +1,43 @@
-const {projects, clients} =require('../sampleData')
+const { projects, clients } = require('../sampleData');
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require('graphql');
 
-const {GraphQLObjectType}=require('graphql')
+// Client Type
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve() {
+        return clients;
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
