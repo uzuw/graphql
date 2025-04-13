3387c607120a08d3ce919e37d3fd625b4763f99f
uzuw <uzuwxd@gmail.com>
2025-04-13 22:58:21 +0545

    added the update mutation to the project data

--- first/server/schema/schema.js
+++ first/server/schema/schema.js
@@ -165,7 +165,39 @@ const mutation= new GraphQLObjectType({
       return Project.findByIdAndDelete(args.id)
     }
 
-  }   
+  },
+
+  //update the project
+  updateProject:{
+    type:ProjectType,
+    args:{
+      id:{type:GraphQLNonNull(GraphQLID)},
+      name:{type:GraphQLString},
+      description:{type:GraphQLString},
+      status:{type:new GraphQLEnumType({
+        name:'UpdatedProjectStatus',
+        values:{
+          'new': {value: 'Not Started'},
+          'progress': {value: 'In Progress'},
+          'completed': {value: 'Completed'}
+        }
+      }),defaultValue:'Not Started'}
+    },
+    resolve(parent,args){
+      return Project.findByIdAndUpdate(args.id,
+        {
+          $set:{
+            name:args.name,
+            description:args.description,
+            status:args.status,
+          },
+        },
+        {
+            new:true //if doesnot exist then create a new project data
+        }
+      );
+    }
+  }
       }
     })
 
