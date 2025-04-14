6bedfcde4127cb78f17641e1d7399827820f35f0
uzuw <uzuwxd@gmail.com>
2025-04-14 23:58:16 +0545

    adding appollo client

--- first/client/src/App.jsx
+++ first/client/src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
+
+
+const client= new ApolloClient({
+  uri:'http://localhost:5000/graphql',
+  cache:new InMemoryCache,
+});
+
 
 function App() {
 
   return (
       <>
+      <ApolloProvider client={client}>
       <Header/>
+      <div>
+        <h1>This is Apolloclient</h1>
+      </div>
+      </ApolloProvider>
       </>
   )
 }
